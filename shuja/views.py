7f154bc41a1eb21883cebf15af590b4fc2a4c335
Deadshot <shujaakba2020@gmail.com>
2021-06-25 11:10:27 +0500

    Updating views for send_mail

--- shuja/views.py
+++ shuja/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Contact
 from django.contrib import messages
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -16,7 +16,8 @@ def index(request):
         elif message == "":
             messages.warning(request, 'Please Enter your Message')
         else:
-            Contact.objects.create(name=full_name, email=email, message=message)
+            # Contact.objects.create(name=full_name, email=email, message=message)
+            send_mail(full_name, message, email, ['shujaakbar2020@gmail.com'])
             messages.success(request, 'Thanks for reaching me, I have received your Message.')
 
     return render(request, 'shuja/index.html')
